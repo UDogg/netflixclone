674f7843b176ef66dad4e75294c3ce6b49ac00fe
Utkarsh Choudhary <72992574+UDogg@users.noreply.github.com>
2023-06-14 12:45:23 +0530

    done with profiles

--- netflix-clone/pages/profiles.tsx
+++ netflix-clone/pages/profiles.tsx
@@ -1,5 +1,7 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export async function getServerSideProps(context: NextPageContext) {
     const session = await getSession(context);
@@ -15,9 +17,46 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Profiles = () => {
+    const { data: user } = useCurrentUser();
+    const router = useRouter();
   return (
-    <div> 
-       <p className="text-white text-4xl">Profiles</p> 
+    <div className="flex items-center h-full justify-center"> 
+        <div className="flex flex-fol">
+            <h1 className="text-3xl md:text-6xl text-white text-center">Who is watching?</h1>
+            <div className="flex items-center justify-center gap-8 mt-18">
+                <div onClick={() => router.push("/")}>
+
+                    <div className="group flex-row w-44 mx-auto">
+                        <div
+                        className="
+                            w-44
+                            h-44
+                            rounded-md
+                            flex
+                            items-center
+                            border-2
+                            border-transparent
+                            group-hover:cursor-pointer
+                            group-hover: border-white
+                            overflow-hidden
+                            "
+                        >
+                            <img src="/images/default-blue.png" alt="Profile" />
+                        </div>
+                        <div className="
+                            mt-4
+                            text-gray-400
+                            text-2xl
+                            text-center
+                            group-hover: text-white
+                        ">
+                            {user?.name}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>
   )
 };
