7da3b5f127eb3a7ee9f3793bb8596d9b0d4cd6ae
Utkarsh Choudhary <72992574+UDogg@users.noreply.github.com>
2023-06-14 12:32:43 +0530

    imported types and swr and created profile route.

--- netflix-clone/hooks/useCurrentUser.ts
+++ netflix-clone/hooks/useCurrentUser.ts
@@ -1,9 +1,10 @@
-import useSwr from 'swr'
-
-import fetcher from '@/libs/fetcher';
+import useSWR from 'swr';
+// swr is similar to react query
+// by using swr, we do not need redux or any state management to fetch data.
+import fetcher from '@/lib/fetcher';
 
 const useCurrentUser = () => {
-  const { data, error, isLoading, mutate } = useSwr('/api/current', fetcher);
+  const { data, error, isLoading, mutate } = useSWR('/api/current', fetcher);
   return {
     data,
     error,
